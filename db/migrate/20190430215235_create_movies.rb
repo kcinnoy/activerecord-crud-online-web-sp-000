f90358f918b6027f997b5afe218f8ed779b70156
Yonnick <yonnick00@gmail.com>
2019-04-30 22:17:42 +0000

    Automatically backed up by Learn

--- db/migrate/20190430215235_create_movies.rb
+++ db/migrate/20190430215235_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :titles
+      t.string :title
       t.string :release_date
       t.string :director
       t.string :lead

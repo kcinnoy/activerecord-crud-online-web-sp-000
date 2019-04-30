8f3acae6d6099e22701ab28417f9be29e7662f8a
Yonnick <yonnick00@gmail.com>
2019-04-30 21:56:28 +0000

    Automatically backed up by Learn

--- db/migrate/20190430215235_create_movies.rb
+++ db/migrate/20190430215235_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    create_table :haunted_houses do |t|
+    create_table :movies do |t|
       t.string :titles
       t.string :release_date
       t.string :director
